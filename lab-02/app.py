2bd7bab86409a702c3f315a6ab74c03a9d8bf7fa
socdef <thaihoang.congviec@gmail.com>
2025-05-27 10:15:04 +0700

    buoi3

--- lab-02/app.py
+++ lab-02/app.py
@@ -1,17 +1,16 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template
 from cipher.caesar import CaesarCipher
 from cipher.vigenere import VigenereCipher
 from cipher.railfence import RailFenceCipher
 from cipher.playfair import PlayFairCipher
 from cipher.transposition import TranspositionCipher
 
-
 app = Flask(__name__)
 
+# TRANG GIAO DIỆN CHÍNH
 @app.route("/")
 def home():
     return render_template('index.html')
-
 
 @app.route("/caesar")
 def caesar():
@@ -33,15 +32,14 @@ def playfair():
 def transposition():
     return render_template('transposition.html')
 
-
-# CAESAR
+# ===== CAESAR CIPHER =====
 @app.route("/caesar/encrypt", methods=["POST"])
 def caesar_encrypt():
     text = request.form['InputPlainText']
     key = int(request.form['InputKeyPlain'])
     Caesar = CaesarCipher()
     encrypted_text = Caesar.encrypt(text, key)
-    return f"text:{text}<br/>key: {key}<br/>encrypted text: {encrypted_text}"
+    return f"text: {text}<br/>key: {key}<br/>encrypted text: {encrypted_text}"
 
 @app.route("/caesar/decrypt", methods=["POST"])
 def caesar_decrypt():
@@ -51,14 +49,14 @@ def caesar_decrypt():
     decrypted_text = Caesar.decrypt(text, key)
     return f"text: {text}<br/>key: {key}<br/>decrypted text: {decrypted_text}"
 
-# VIGENERE
+# ===== VIGENERE CIPHER =====
 @app.route("/vigenere/encrypt", methods=["POST"])
 def vigenere_encrypt():
     text = request.form['plain_text']
     key = request.form['key']
     vigenere = VigenereCipher()
     encrypted_text = vigenere.vigenere_encrypt(text, key)
-    return f"text:{text}<br/>key: {key}<br/>encrypted text: {encrypted_text}"
+    return f"text: {text}<br/>key: {key}<br/>encrypted text: {encrypted_text}"
 
 @app.route("/vigenere/decrypt", methods=["POST"])
 def vigenere_decrypt():
@@ -68,14 +66,14 @@ def vigenere_decrypt():
     decrypted_text = vigenere.vigenere_decrypt(text, key)
     return f"text: {text}<br/>key: {key}<br/>decrypted text: {decrypted_text}"
 
-# RAILFENCE
+# ===== RAIL FENCE CIPHER =====
 @app.route("/railfence/encrypt", methods=["POST"])
 def railfence_encrypt():
     text = request.form['plain_text']
     key = int(request.form['key'])
     railfence = RailFenceCipher()
     encrypted_text = railfence.rail_fence_encrypt(text, key)
-    return f"text:{text}<br/>key: {key}<br/>encrypted text: {encrypted_text}"
+    return f"text: {text}<br/>key: {key}<br/>encrypted text: {encrypted_text}"
 
 @app.route("/railfence/decrypt", methods=["POST"])
 def railfence_decrypt():
@@ -85,7 +83,7 @@ def railfence_decrypt():
     decrypted_text = railfence.rail_fence_decrypt(text, key)
     return f"text: {text}<br/>key: {key}<br/>decrypted text: {decrypted_text}"
 
-# PLAYFAIR
+# ===== PLAYFAIR CIPHER =====
 @app.route("/playfair/creatematrix", methods=["POST"])
 def playfair_create_matrix():
     key = request.form['key']
@@ -93,14 +91,31 @@ def playfair_create_matrix():
     matrix = playfair.playfair_creatematrix(key)
     return f"Matrix for key '{key}':<br/>{matrix}"
 
-# TRANSPOSITION
+@app.route("/playfair/encrypt", methods=["POST"])
+def playfair_encrypt():
+    text = request.form['plain_text']
+    key = request.form['key']
+    playfair = PlayFairCipher()
+    encrypted_text = playfair.playfair_encrypt(text, key)
+    return f"text: {text}<br/>key: {key}<br/>encrypted text: {encrypted_text}"
+
+@app.route("/playfair/decrypt", methods=["POST"])
+def playfair_decrypt():
+    text = request.form['cipher_text']
+    key = request.form['key']
+    playfair = PlayFairCipher()
+    decrypted_text = playfair.playfair_decrypt(text, key)
+    return f"text: {text}<br/>key: {key}<br/>decrypted text: {decrypted_text}"
+
+
+# ===== TRANSPOSITION CIPHER =====
 @app.route("/transposition/encrypt", methods=["POST"])
 def transposition_encrypt():
     text = request.form['plain_text']
     key = int(request.form['key'])
     transposition = TranspositionCipher()
     encrypted_text = transposition.encrypt(text, key)
-    return f"text:{text}<br/>key: {key}<br/>encrypted text: {encrypted_text}"
+    return f"text: {text}<br/>key: {key}<br/>encrypted text: {encrypted_text}"
 
 @app.route("/transposition/decrypt", methods=["POST"])
 def transposition_decrypt():
@@ -109,8 +124,6 @@ def transposition_decrypt():
     transposition = TranspositionCipher()
     decrypted_text = transposition.decrypt(text, key)
     return f"text: {text}<br/>key: {key}<br/>decrypted text: {decrypted_text}"
-
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050, debug=True)
--- lab-02/cipher/playfair/playfair_cipher.py
+++ lab-02/cipher/playfair/playfair_cipher.py
@@ -1,82 +1,89 @@
 class PlayFairCipher:
-    def __init__(self) -> None:
-        pass
-
     def __init__(self):
         pass
 
     def create_playfair_matrix(self, key):
-        key = key.replace("J", "I")
-        key = key.upper()
-        key_set = set(key)
-        alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
-        remaining_letters = [
-            letter for letter in alphabet if letter not in key_set
-        ]
-        matrix = list(key)
-        for letter in remaining_letters:
-            matrix.append(letter)
-            if len(matrix) == 25:
-                break
-        playfair_matrix = [matrix[i:i + 5] for i in range(0, len(matrix), 5)]
-        return playfair_matrix
+        key = key.upper().replace("J", "I")
+        result = []
+        used = set()
+
+        for char in key:
+            if char not in used and char.isalpha():
+                used.add(char)
+                result.append(char)
+
+        for char in "ABCDEFGHIKLMNOPQRSTUVWXYZ":
+            if char not in used:
+                used.add(char)
+                result.append(char)
+
+        matrix = [result[i:i + 5] for i in range(0, 25, 5)]
+        return matrix
 
     def find_letter_coords(self, matrix, letter):
-        for row in range(len(matrix)):
-            for col in range(len(matrix[row])):
+        for row in range(5):
+            for col in range(5):
                 if matrix[row][col] == letter:
                     return row, col
+        return None, None
 
-    def playfair_encrypt(self, plain_text, matrix):
-        plain_text = plain_text.replace("J", "I")
-        plain_text = plain_text.upper()
+    def prepare_plaintext(self, text):
+        text = text.upper().replace("J", "I")
+        result = ""
+        i = 0
+        while i < len(text):
+            a = text[i]
+            b = text[i + 1] if i + 1 < len(text) else "X"
+            if a == b:
+                result += a + "X"
+                i += 1
+            else:
+                result += a + b
+                i += 2
+        if len(result) % 2 != 0:
+            result += "X"
+        return result
+
+    def playfair_encrypt(self, plain_text, key):
+        matrix = self.create_playfair_matrix(key)
+        plain_text = self.prepare_plaintext(plain_text)
         encrypted_text = ""
 
         for i in range(0, len(plain_text), 2):
-            pair = plain_text[i:i + 2]
-            if len(pair) == 1:
-                pair += "X"
-
-            row1, col1 = self.find_letter_coords(matrix, pair[0])
-            row2, col2 = self.find_letter_coords(matrix, pair[1])
+            a, b = plain_text[i], plain_text[i + 1]
+            row1, col1 = self.find_letter_coords(matrix, a)
+            row2, col2 = self.find_letter_coords(matrix, b)
 
             if row1 == row2:
-                encrypted_text += matrix[row1][(col1 + 1) % 5] + matrix[row2][(col2 + 1) % 5]
+                encrypted_text += matrix[row1][(col1 + 1) % 5]
+                encrypted_text += matrix[row2][(col2 + 1) % 5]
             elif col1 == col2:
-                encrypted_text += matrix[(row1 + 1) % 5][col1] + matrix[(row2 + 1) % 5][col2]
+                encrypted_text += matrix[(row1 + 1) % 5][col1]
+                encrypted_text += matrix[(row2 + 1) % 5][col2]
             else:
-                encrypted_text += matrix[row1][col2] + matrix[row2][col1]
+                encrypted_text += matrix[row1][col2]
+                encrypted_text += matrix[row2][col1]
+
         return encrypted_text
 
-    def playfair_decrypt(self, cipher_text, matrix):
-        cipher_text = cipher_text.upper()
+    def playfair_decrypt(self, cipher_text, key):
+        matrix = self.create_playfair_matrix(key)
+        cipher_text = cipher_text.upper().replace("J", "I")
         decrypted_text = ""
-        decrypted_text1 = ""
 
         for i in range(0, len(cipher_text), 2):
-            pair = cipher_text[i:i + 2]
-
-            row1, col1 = self.find_letter_coords(matrix, pair[0])
-            row2, col2 = self.find_letter_coords(matrix, pair[1])
+            a, b = cipher_text[i], cipher_text[i + 1]
+            row1, col1 = self.find_letter_coords(matrix, a)
+            row2, col2 = self.find_letter_coords(matrix, b)
 
             if row1 == row2:
-                decrypted_text += matrix[row1][(col1 - 1) % 5] + matrix[row2][(col2 - 1) % 5]
+                decrypted_text += matrix[row1][(col1 - 1) % 5]
+                decrypted_text += matrix[row2][(col2 - 1) % 5]
             elif col1 == col2:
-                decrypted_text += matrix[(row1 - 1) % 5][col1] + matrix[(row2 - 1) % 5][col2]
+                decrypted_text += matrix[(row1 - 1) % 5][col1]
+                decrypted_text += matrix[(row2 - 1) % 5][col2]
             else:
-                decrypted_text += matrix[row1][col2] + matrix[row2][col1]
-        banro = ""
+                decrypted_text += matrix[row1][col2]
+                decrypted_text += matrix[row2][col1]
 
-        for i in range(0, len(decrypted_text) - 2, 2):
-            if decrypted_text[i] == decrypted_text[i + 2]:
-                banro += decrypted_text[i]
-            else:
-                banro += decrypted_text[i] + "" + decrypted_text[i + 1]
-        if decrypted_text[-1] == "X":
-            banro += decrypted_text[-2]
-        else:
-            banro += decrypted_text[-2]
-            banro += decrypted_text[-1]
-        return banro
-
-
+        return decrypted_text
